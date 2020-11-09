7c85745ea4a980ff46152915dc902a4256416f32
Ver <vpasamic22@gmail.com>
2020-11-08 23:13:09 -0800

    borders

--- src/components/TableContents.js
+++ src/components/TableContents.js
@@ -32,12 +32,12 @@ function TableContents(props) {
             {employees.map(employee => (
                 <div className="row d-flex align-items-center" key={employee.email}>
                     <div className="col-1"><img src={employee.picture.medium} alt="Emp Img" /></div>
-                    <div className="col-1">{employee.name.first}</div>
-                    <div className="col-1">{employee.name.last}</div>
-                    <div className="col-2">{employee.dob.date.split("T")[0]}</div>
-                    <div className="col-2">{employee.location.city}</div>
-                    <div className="col-2">{employee.phone}</div>
-                    <div className="col-3">{employee.email}</div>
+                    <div className="col-1 border-right">{employee.name.first}</div>
+                    <div className="col-1 border-right">{employee.name.last}</div>
+                    <div className="col-2 border-right">{employee.dob.date.split("T")[0]}</div>
+                    <div className="col-2 border-right">{employee.location.city}</div>
+                    <div className="col-2 border-right">{employee.phone}</div>
+                    <div className="col-3 border-right">{employee.email}</div>
                 </div>
             ))}
         </div>
